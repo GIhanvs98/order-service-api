5d1d4aed4a3ccc077d3bb1b343d7fe8b00d9f4af
Gihanvs98 <darkcreationsl98@gmail.com>
2025-04-07 21:46:29 +0530

    order service interface create and impl

---
+++ src/main/java/com/gihanvs/quickcart/order_service_api/service/impl/CustomerOrderServiceImpl.java
@@ -0,0 +1,13 @@
+package com.gihanvs.quickcart.order_service_api.service.impl;
+
+import com.gihanvs.quickcart.order_service_api.dto.request.CustomerOrderRequestDto;
+import com.gihanvs.quickcart.order_service_api.service.CustomerOrderService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerOrderServiceImpl implements CustomerOrderService {
+    @Override
+    public void createOrder(CustomerOrderRequestDto requestDto) {
+
+    }
+}
